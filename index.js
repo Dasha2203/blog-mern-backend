e00101774f396ef594211843fe5469a55fa7843d
Дарья Корбут <darakorbut@MacBook-Pro-Dara.local>
2022-07-17 10:28:43 +0300

    update port

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer  = require('multer');
 const cors = require('cors');
-
+const fs = require('fs');
 const { postController, userController } = require("./controllers/index");
 const { checkAuth, handleValidationErrors } = require("./utils/index");
 const { registerValidation, loginValidation, postCreateValidation } = require("./validations/index");
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('db connect')
+    console.log('db ok ,')
   });
 
 const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
     cb(null, file.originalname);
-  }
-})
+  },
+});
 
 const upload = multer({storage});
 
@@ -47,7 +50,7 @@ app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, post
 app.delete('/posts/:id', checkAuth, postController.remove);
 app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, postController.update);
 
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(process.env.PORT || 5555, (err) => {
   if (err) {
     console.log('error', error)
     return
